fd33abddfdea43e5e33a2fc596b4b71ec7b64949
Emanuele <manu.pogliari@gmail.com>
2024-08-19 18:48:29 +0200

    db created

---
+++ database/schema.sql
@@ -0,0 +1,31 @@
+-- Create trips table
+CREATE TABLE trips (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT
+    start_date DATE
+);
+
+--create days table
+CREATE TABLE days {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    trip_id INT,
+    day_number INT NOT NULL,
+    date DATE,
+    -- Add foreign key constraint to trips table
+    FOREIGN KEY (trip_id) REFERENCES trips(id) ON DELETE CASCADE
+};
+--create stages table
+CREATE TABLE stages {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    day_id INT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    location VARCHAR(255),
+    image VARCHAR(255),
+    latitude DECIMAL(10, 7),
+    longitude DECIMAL(10, 7),
+    -- Add foreign key constraint to days table 
+    FOREIGN KEY (day_id) REFERENCES days(id) ON DELETE CASCADE
+};
+
